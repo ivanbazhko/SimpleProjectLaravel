20d4d5628f00bf957767d96664c7e06a57abb023
ivanbazhko <ivanbazhko3@gmail.com>
2020-09-10 19:01:56 +0300

    message'

--- app/Http/Controllers/PlaneController.php
+++ app/Http/Controllers/PlaneController.php
@@ -31,49 +31,60 @@ class PlaneController extends Controller
 
         $planes = Plane::all();
 
+        $having = false;
         if($req->has('name')){
+            $having = true;
             $planes = Plane::Name($req->input('name'));
          }
          
         if($req->has('manufacture')){
-            if($req->input('manufacture') !== 'any'){
+            if($req->input('manufacture') != 'any'){
+            $having = true;
             $planes = Plane::From($req->input('manufacture'));
         }
         }
 
         if($req->has('pricefrom')){
+            $having = true;
             $planes = Plane::Pricef($req->input('pricefrom'));
         }
 
         if($req->has('priceto')){
+            $having = true;
             $planes = Plane::Pricet($req->input('priceto'));
         }
 
         if($req->has('paxfrom')){
-           $planes = Plane::Paxf($req->input('paxfrom'));
+            $having = true;
+            $planes = Plane::Paxf($req->input('paxfrom'));
         }
 
         if($req->has('paxto')){
+            $having = true;
             $planes = Plane::Paxt($req->input('paxto'));
          }
 
         if($req->has('fuselage')){
-            if($req->input('fuselage') !== 'any'){
+            if($req->input('fuselage') != 'any'){
+            $having = true;
             $planes = Plane::Fus($req->input('fuselage'));
         }
         }
 
         if($req->has('minrange')){
+            $having = true;
             $planes = Plane::Minr($req->input('minrange'));
         }
 
+        if($having){
         $planes = $planes->get();
+        }
 
         return view('store', ['dataPlanes' => $planes]);
     }
 
     public function onlyOne($id) {
-        $plane = Plane::where('id', $id)->get();
+        $plane = Plane::findOrFail($id);
         return view('onlyOne', ['th' => $plane]);
     }
 }
